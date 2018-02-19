387e75e625459d603b8bf385ef687093251aa3d8
Pavel Popov <pavelpopov@outlook.com>
2018-02-19 13:46:08 +0100

    Update fzy_select_directory.fish

--- functions/fzy_select_directory.fish
+++ functions/fzy_select_directory.fish
@@ -9,10 +9,8 @@ function __fzy_get_directory_list -d 'Get a list of directories using fd or find
   end
 end
 
-function fzy_select_directory -d 'cd to a directory using fzy'
+function fzy_select_directory -d 'select directory using fzy'
   __fzy_get_directory_list | fzy | read -l foo
-  if [ $foo ]
-    cd $foo
-  end
   commandline -f repaint
+  commandline $foo
 end
